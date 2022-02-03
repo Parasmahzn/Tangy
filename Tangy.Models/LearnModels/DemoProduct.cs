21f0c52d14eea818d2340a414c21c5d0b9dbf044
Paras Maharjan <paras.maharjan@nepalpayment.com>
2022-02-03 20:48:47 +0545

    .

---
+++ Tangy.Models/LearnModels/DemoProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy.Models.LearnModels
+{
+    public class DemoProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public decimal Price { get; set; }
+        public List<DemoProductProp> ProductProperties { get; set; }
+
+    }
+}
